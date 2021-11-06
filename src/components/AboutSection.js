b783e855b034dcdfc423637fd2af4f2e786a4193
Mika <dmitriygolubapk@gmail.com>
2021-11-06 12:41:54 +0300

    4. FAQ Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,5 @@
 import home1 from '../img/home1.png';
-import styled from 'styled-components';
+import {About, Description, Hide, Image} from '../components/styles'
 
 const AboutSection = () => {
 	return (
@@ -28,36 +28,7 @@ const AboutSection = () => {
 
 // Styled Components
 
-const About = styled.div`
-	min-height: 90vh;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 5rem 0rem 0rem 10rem;
-	color: white;
-`;
 
-const Description = styled.div`
-	/* flex: 1; */
-	padding-right: 5rem;
-	h2 {
-		font-weight: lighter;
-	}
-`
-
-const Image = styled.div`
-	flex: 1;
-	overflow: hidden;
-	img {
-		width: 130%;
-		height: 120vh;
-		object-fit: cover;
-	}
-`
-
-const Hide = styled.div`
-	overflow: hidden;
-`
 
 
 export default AboutSection;
